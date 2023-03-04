5bfd678eb613a1c66ed872291863b515004c97ac
Natalia <nataliahola@yandex.ru>
2023-03-04 10:54:17 +0300

    добавлена программа

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿void ShowArray(string [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+    string[] finalArray = {"hello", "2", "world", ":-)"};
+    //string[] finalArray = {"1234", "1567", "-2", "computer science"};
+    //string[] finalArray = {"Russia", "Denmark", "Kazan"};
+    string[] emptyArray = new string [4];
+
+
+    for (int i=0; i<finalArray.Length; i++)
+    {
+      if (finalArray[i].Length <= 3)
+     {
+      emptyArray[i] = finalArray[i];
+
+      }
+    }
+
+  ShowArray(emptyArray);
